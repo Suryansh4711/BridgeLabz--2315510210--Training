52d92f83a40a124e52ecb688dc6f9f7c177b373f
Suryansh Agarwal <agsuryansh4711@gmail.com>
2025-08-15 22:23:00 +0530

    add multiple programs for various calculations including even/odd identification, greatest factor, multiples, power calculation, and FizzBuzz

--- ControlFlow_Practices/Level2/quest7.java
+++ ControlFlow_Practices/Level2/quest7.java
@@ -2,23 +2,22 @@ package ControlFlow_Practices.Level2;
 
 import java.util.Scanner;
 
-public class quest1 {
+public class quest7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.print("Enter a natural number: ");
+        System.out.print("Enter a positive integer: ");
         int number = input.nextInt();
         
         if (number > 0) {
+            System.out.println("Factors of " + number + " are:");
             for (int i = 1; i <= number; i++) {
-                if (i % 2 == 0) {
-                    System.out.println(i + " is even number");
-                } else {
-                    System.out.println(i + " is odd number");
+                if (number % i == 0) {
+                    System.out.println(i);
                 }
             }
         } else {
-            System.out.println("Please enter a natural number");
+            System.out.println("Please enter a positive integer");
         }
         input.close();
     }
